43ab9297babab728440c106a3daceacf68c5e24e
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-08-01 21:12:22 -0500

    commit
    class

---
+++ src/ec/com/asofar/prestaciones/tarifarios/prestaciones_auto_tarifarios.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.asofar.prestaciones.tarifarios;
+
+
+public class prestaciones_auto_tarifarios {
+    private Integer idprestaciones;
+    private String productosComprados;
+    private String prestacion;
+    private String facturable;
+    private double iva;
+    private double descuento;
+
+    public Integer getIdprestaciones() {
+        return idprestaciones;
+    }
+
+    public void setIdprestaciones(Integer idprestaciones) {
+        this.idprestaciones = idprestaciones;
+    }
+
+    public String getProductosComprados() {
+        return productosComprados;
+    }
+
+    public void setProductosComprados(String productosComprados) {
+        this.productosComprados = productosComprados;
+    }
+
+    public String getPrestacion() {
+        return prestacion;
+    }
+
+    public void setPrestacion(String prestacion) {
+        this.prestacion = prestacion;
+    }
+
+    public String getFacturable() {
+        return facturable;
+    }
+
+    public void setFacturable(String facturable) {
+        this.facturable = facturable;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+    
+    
+    
+    
+    
+}
--- src/ec/com/asofar/views/prestacion/tarifario/Tarifarios.java
+++ src/ec/com/asofar/views/prestacion/tarifario/Tarifarios.java
@@ -33,11 +33,11 @@ public class Tarifarios extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 703, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 341, Short.MAX_VALUE)
         );
 
         pack();
