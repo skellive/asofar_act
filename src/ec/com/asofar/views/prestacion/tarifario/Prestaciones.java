6df06811df7e6622664c89b115be419f5100ddc2
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-08-02 16:08:23 -0500

    commit
    tarifario

--- src/ec/com/asofar/views/prestacion/tarifario/Prestaciones.java
+++ src/ec/com/asofar/views/prestacion/tarifario/Prestaciones.java
@@ -85,9 +85,7 @@ public class Prestaciones extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
